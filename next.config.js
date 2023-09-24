c8760497bd656ddb55ccd63ca43e148551c725b1
outoftheboxdev1 <130626635+outoftheboxdev1@users.noreply.github.com>
2023-09-24 00:43:07 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: false,
   images: {
-    domains: ['ipfs.infura.io', 'ever-traded.infura-ipfs.io', 'localhost'],
+    domains: ['ipfs.infura.io', 'ever-traded.infura-ipfs.io', 'localhost', 'evertraded.com', 'evertraded.vercel.app'],
   },
 };
