bc98f4824ed1dbaa9eca44f7c043c34e4d959b25
outoftheboxdev1 <130626635+outoftheboxdev1@users.noreply.github.com>
2023-09-25 16:15:42 -0400

    Update uploadProfilePic.js

--- pages/api/uploadProfilePic.js
+++ pages/api/uploadProfilePic.js
@@ -1,8 +1,10 @@
 import { put } from '@vercel/blob';
 
 export default async function handler(request, response) {
+  const fileExtension = request.query.filename.split('.').pop(); // Get the file extension
   const blob = await put(request.query.filename, request.body, {
     access: 'public',
+    contentDisposition: `attachment; filename="${request.query.user}${fileExtension}"`,
   });
 
   return response.status(200).json(blob);
