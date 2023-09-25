c03b3be168185e9bdcdcf8fc917b82996e05820a
outoftheboxdev1 <130626635+outoftheboxdev1@users.noreply.github.com>
2023-09-25 15:52:25 -0400

    Update uploadProfilePic.js

--- pages/api/uploadProfilePic.js
+++ pages/api/uploadProfilePic.js
@@ -1,13 +1,22 @@
 import { put } from '@vercel/blob';
 
 export default async function handler(request, response) {
-  const { searchParams } = new URL(request.url);
-  const currentAccount = searchParams.get('user');
-  const file = request.files.file[0];
-  const filename = `${currentAccount}.png`;
+  try {
+    const { searchParams } = new URL(request.url);
+    const currentAccount = searchParams.get('user');
+    const file = request.files.file[0];
+    const fileExtension = file.name.split('.').pop(); // Get the file extension
+    const filename = `${currentAccount}.${fileExtension}`;
 
-  const blob = await put(filename, file, {
-    access: 'public',
-  });
-  return response.status(200).json(blob);
+    const blob = await put(filename, file, {
+      access: 'public',
+    });
+
+    // Respond with a JSON object containing the blob URL
+    return response.status(200).json({ success: true, blobUrl: blob.url });
+  } catch (error) {
+    // Handle any errors that occur during the upload process
+    console.error('Error uploading file:', error);
+    return response.status(500).json({ success: false, message: 'File upload failed' });
+  }
 }
